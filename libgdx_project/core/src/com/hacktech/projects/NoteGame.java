7429bd5b193d55018733a4ccdbf9050b7d2ac6b1
Danny Flax <dannyflax@Dannys-MacBook-Pro.local>
2016-02-27 00:24:37 -0800

    added basic time management

--- libgdx_project/core/src/com/hacktech/projects/NoteGame.java
+++ libgdx_project/core/src/com/hacktech/projects/NoteGame.java
@@ -5,10 +5,13 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.TimeUtils;
 
 public class NoteGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	
+	long startTime = TimeUtils.millis();
 	
 	@Override
 	public void create () {
@@ -16,8 +19,14 @@ public class NoteGame extends ApplicationAdapter {
 		img = new Texture("badlogic.jpg");
 	}
 
+	
+	
 	@Override
 	public void render () {
+		long timeElapsed = TimeUtils.millis() - startTime;
+		
+		System.out.println(timeElapsed);
+		
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
